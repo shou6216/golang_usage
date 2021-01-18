a38aab544ba5bad7f84b36bc7e678ad834e94ab1
s-yoshino <shou-y6216@yoshinoda.com>
2021-01-19 00:04:48 +0900

    [update] 2021/01/19学習分追加

--- hoge.go
+++ hoge.go
@@ -3,7 +3,16 @@ package main
 import (
 	"fmt"
 	"os/user"
+	"strconv"
+	"strings"
 	"time"
+)
+
+const Pi = 3.14
+
+const (
+	Username = "test_user"
+	Password = "test_pass"
 )
 
 // 最初に自動で呼ばれる関数
@@ -41,4 +50,47 @@ func main() {
 	xt, xf := true, false
 	fmt.Println(xi, xf64, xs, xt, xf)
 	fmt.Printf("%T", xf64)
+
+	fmt.Println(Pi, Username, Password)
+
+	var (
+		u8  uint8     = 255
+		i8  int8      = 127
+		f32 float32   = 0.2
+		c64 complex64 = -5 + 12i
+	)
+	fmt.Println(u8, i8, f32, c64)
+	fmt.Printf("type=%T value=%v", u8, u8)
+
+	fmt.Println(string("Hello World"[0]))
+
+	var ss string = "Hello World"
+	fmt.Println(strings.Replace(ss, "H", "X", 1))
+	fmt.Println(ss)
+
+	fmt.Println("\"")
+	fmt.Println(`"`)
+
+	var s3 string = "14"
+	// _ 利用しなくてもエラーにならないようにする表記
+	i3, _ := strconv.Atoi(s3)
+	fmt.Printf("%T %v", i3, i3)
+
+	var a4 [2]int
+	a4[0] = 100
+	a4[1] = 200
+	fmt.Println(a4)
+	var b4 [2]int = [2]int{300, 400}
+	fmt.Println(b4)
+
+	n5 := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n5)
+	fmt.Println(n5[2])
+	fmt.Println(n5[2:4])
+	fmt.Println(n5[:2])
+	fmt.Println(n5[2:])
+	fmt.Println(n5[:])
+
+	n5 = append(n5, 1000, 2000, 3000, 4000)
+	fmt.Println(n5)
 }
