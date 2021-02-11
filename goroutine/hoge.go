3c84f8b43766783ed62c0285d1868f30466a78ea
s-yoshino <shou-y6216@yoshinoda.com>
2021-02-11 23:53:53 +0900

    [update] goroutine完了

--- goroutine/hoge.go
+++ goroutine/hoge.go
@@ -60,6 +60,15 @@ func goroutine5(ch chan string) {
 	}
 }
 
+func goroutine6(s []string, c chan string) {
+	sum := ""
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
+
 func producer(ch chan int, i int) {
 	ch <- i * 2
 }
@@ -92,6 +101,23 @@ func multi4(second <-chan int, third chan<- int) {
 	for i := range second {
 		third <- i * 4
 	}
+}
+
+type Counter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (c *Counter) Inc(key string) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.v[key]++
+}
+
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
 }
 
 func main() {
@@ -186,5 +212,48 @@ func main() {
 		case msg2 := <-c255:
 			fmt.Println(msg2)
 		}
+		break
+	}
+
+	tick56 := time.Tick(100 * time.Microsecond)
+	boom56 := time.After(500 * time.Microsecond)
+OuterLoop:
+	for {
+		select {
+		case t := <-tick56:
+			fmt.Println("tick.", t)
+		case <-boom56:
+			fmt.Println("BOOM!")
+			// ここで普通にbreakしても処理は終わらない
+			// loopを指定する
+			break OuterLoop
+
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Microsecond)
+		}
+	}
+	fmt.Println("##########################")
+
+	// Mutex
+	c57 := Counter{v: make(map[string]int)}
+	go func() {
+		for i := 0; i < 10; i++ {
+			c57.Inc("key")
+		}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			c57.Inc("key")
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	fmt.Println(c57.Value("key"))
+
+	words58 := []string{"test1!", "test2!", "test3!", "test4!"}
+	c58 := make(chan string)
+	go goroutine6(words58, c58)
+	for w := range c58 {
+		fmt.Println(w)
 	}
 }
